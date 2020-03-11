6ced5db2085cb54e4cc035782342b1e0973bcd5a
onteriowright <onteriowright@yahoo.com>
2020-03-11 09:39:42 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace TryCatchIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int difficulty = 0;
+
+            while (true)
+            {
+                Console.Write("What is the bank difficulty? ");
+                var userInput = Console.ReadLine();
+                if (userInput == "")
+                {
+                    Console.WriteLine("Closing App!");
+                    break;
+                }
+                try
+                {
+
+                    difficulty = int.Parse(userInput);
+
+                    var total = 100 / difficulty;
+                    Console.WriteLine(total);
+
+                }
+                catch (System.Exception ex)
+                {
+                    Console.WriteLine($"Oops something went wrong! {ex.Message}");
+                }
+            }
+
+        }
+    }
+}
